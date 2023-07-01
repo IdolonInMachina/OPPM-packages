6089a0ae328b62bd49a97058a83a467bb4d565f8
IdolonInMachina <Cooperbar@gmail.com>
2023-07-02 00:23:01 +1000

    Improves install file

--- station-control/install.lua
+++ station-control/install.lua
@@ -1,12 +1,23 @@
 
+-- Create folder if it doesn't exist
 os.execute("mkdir -p /usr/bin/station-control")
+
+-- Remove any old lua files in the directory
+os.execute("rm -f /usr/bin/station-control/*.lua")
+
+-- Download new files
 os.execute("wget https://raw.githubusercontent.com/IdolonInMachina/OPPM-packages/master/station-control/dist/main-controller.lua -O /usr/bin/station-control/main-controller.lua")
-os.execute("wget https://raw.githubusercontent.com/IdolonInMachina/OPPM-packages/master/station-control/dist/lualib_bundle.lua -O /usr/bin/station-control/lualib_bundle.lua")
 os.execute("wget https://raw.githubusercontent.com/IdolonInMachina/OPPM-packages/master/station-control/dist/main.lua -O /usr/bin/station-control/main.lua")
 os.execute("wget https://raw.githubusercontent.com/IdolonInMachina/OPPM-packages/master/station-control/dist/relay-node.lua -O /usr/bin/station-control/relay-node.lua")
 os.execute("wget https://raw.githubusercontent.com/IdolonInMachina/OPPM-packages/master/station-control/dist/station-node.lua -O /usr/bin/station-control/station-node.lua")
+
+-- Download  library files
+os.execute("wget https://raw.githubusercontent.com/IdolonInMachina/OPPM-packages/master/station-control/dist/lualib_bundle.lua -O /usr/bin/station-control/lualib_bundle.lua")
 os.execute("wget https://raw.githubusercontent.com/IdolonInMachina/OPPM-packages/master/station-control/src/json.lua -O /usr/bin/station-control/json.lua")
 
+-- Copy library files to /lib
+os.execute("cp /usr/bin/station-control/lualib_bundle.lua /lib/lualib_bundle.lua")
+os.execute("cp /usr/bin/station-control/json.lua /lib/json.lua")
 
 
 local file_path = "/home/autorun.lua"
